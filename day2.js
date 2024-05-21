837d2fa735b25675b8617c518ddc63f2581cb47a
Siddhesh Milind Wandile <siddheshwandile@gmail.com>
2024-05-21 12:17:31 +0530

    day 2

---
+++ day2.js
@@ -0,0 +1,23 @@
+//*********Create password
+
+// let name = prompt("Enter a Name:");
+
+// console.log(`Your Password is: \n@${name}${name.length}`);
+
+//*****Arrays */
+
+// let arr = [1, 2, 3, 4, 5, 6];
+
+// console.log(arr);
+// console.log(arr.length);
+
+/****Avg Number of Array *****/
+
+// let num = [93, 58, 1, 68, 48, 99, 89, 96];
+// let sum = 0;
+
+// for (let i = 0; i < num.length; i++) {
+//   sum += num[i];
+// }
+
+// console.log("The average is " + sum / num.length);
